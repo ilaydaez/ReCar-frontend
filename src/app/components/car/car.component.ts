5aebc66867c643c9cadecd13fa2e7e5e1ecf59d0
ilaydaez64 <ilaydaez64@gmail.com>
2021-03-11 22:55:20 +0300

    Api bağlantısı

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
+import { HttpClient } from '@angular/common/http';
+import { CarResponseModel } from 'src/app/models/carResponseModel';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-car',
@@ -9,10 +12,19 @@ import { Car } from 'src/app/models/car';
 export class CarComponent implements OnInit {
 
   cars: Car[]=[];
+  apiUrl="https://localhost:44350/api/cars/getall"
+  //carResponseModel:CarResponseModel={};
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  getCars(){
+    this.httpClient.get<CarResponseModel>(this.apiUrl).subscribe(reponse=>{
+      
+    });
+
   }
 
 }
